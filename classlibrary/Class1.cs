226979b0915ddb233014457142e69823f5aaa7bd
unknown <solisluris@gmail.com>
2025-01-14 16:45:18 -0600

    leader send heartbeat to all the folloers/neuighborns

--- classlibrary/Class1.cs
+++ classlibrary/Class1.cs
@@ -1,6 +1,93 @@
-﻿namespace classlibrary;
+﻿using System.Data.Common;
 
-public class Class1
+namespace classlibrary;
+
+public class ServerNode : IServerNode
 {
+    bool _vote { get; set; }
+    List<IServerNode> _neighbors { get; set; }
+    bool _isLeader { get; set; }
+    private Timer _heartbeatTimer;
+    private int _intervalHeartbeat;
+
+    public ServerNode()
+    {
+        _neighbors = new List<IServerNode>();
+        _vote = false;
+        _isLeader = false;
+    }
+
+    public ServerNode(bool vote, List<IServerNode> neighbors = null, int heartbeatInterval = 50)
+    {
+        _vote = vote;
+        _neighbors = neighbors ?? new List<IServerNode>();
+        _isLeader = false;
+        _intervalHeartbeat = heartbeatInterval;
+    }
+
+    public void ReceiveRPC()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SendRPC()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ReturnVote()
+    {
+        return _vote;
+    }
+
+    public bool CountVotesInTheCluster()
+    {
+        int numberOfVotesToWin = _neighbors.Count() / 2;
+        int trueVotes = 0;
+        int falseVotes = 0;
+
+        foreach (ServerNode neighbor in _neighbors)
+        {
+            if (trueVotes == numberOfVotesToWin || falseVotes == numberOfVotesToWin) break;
+
+            if (neighbor.ReturnVote())
+            {
+                trueVotes++;
+            }
+            else
+            {
+                falseVotes++;
+            }
+        }
+
+        return trueVotes >= falseVotes;
+    }
+
+    public void BecomeLeader()
+    {
+        _isLeader = true;
+        _heartbeatTimer = new Timer(Append, null, 0, _intervalHeartbeat);
+    }
+
+    public void Append(object state)
+    {
+        if (_isLeader)
+        {
+            foreach (var neighbor in _neighbors)
+            {
+                neighbor.ReceiveRPC();
+            }
+        }
+    }
+}
+
+public interface IServerNode
+{
+    void SendRPC();
+    void Append(object state);
+
+    void ReceiveRPC();
+    //request
+    //respond 
 
 }
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,10 +1,29 @@
+using classlibrary;
+using NSubstitute;
+using NSubstitute.ReceivedExtensions;
+
 namespace test;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void LeaderSendsHeartbeatsToAllNeighbors()
     {
+        // Arrange
+        var mockNeighbor1 = Substitute.For<IServerNode>();
+        var mockNeighbor2 = Substitute.For<IServerNode>();
+        var mockNeighbors = new List<IServerNode> { mockNeighbor1, mockNeighbor2 };
+        var leader = new ServerNode(true, mockNeighbors);
 
+        // Act
+        leader.BecomeLeader();
+        for (int i = 0; i < 3; i++)
+        {
+            leader.Append(null);
+        }
+
+        // Assert
+        mockNeighbor1.ReceivedWithAnyArgs(4).ReceiveRPC();
+        mockNeighbor2.ReceivedWithAnyArgs(4).ReceiveRPC();
     }
 }
